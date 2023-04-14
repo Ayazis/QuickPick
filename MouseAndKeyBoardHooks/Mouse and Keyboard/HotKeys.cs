870f26dc63cbe26092d7d6a6f28c01be40871188
Floris van de Geest <florisvdgeest@hotmail.com>
2023-04-14 16:51:57 +0200

    - Created net6 app - created new project that handles key events, loosely coupled.

---
+++ MouseAndKeyBoardHooks/Mouse and Keyboard/HotKeys.cs
@@ -0,0 +1,113 @@
+﻿
+using Ayazis.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Ayazis.KeyHooks;
+public static class HotKeys
+{
+	static List<Keys> PressedKeys = new List<Keys>();
+	static List<Keys> _presetKeyCombination = new List<Keys>();
+
+	/// <summary>
+	/// Hooks into the keyboard and the mouse events. 
+	/// </summary>
+	/// <param name="presetKeyCombination">The list of keys that need to be hit for the KeyCombinationHit to be raised.</param>
+	public static void SubscribeToKeyEvents(IEnumerable<Keys> presetKeyCombination)
+	{
+		// Set predefined KeyCombination.
+		_presetKeyCombination = presetKeyCombination.ToList();
+
+		// Connect House and Keyboard input events to current class.
+		CaptureKeyBoardAndMouse.HookIntoMouseAndKeyBoard();
+		CaptureKeyBoardAndMouse.KeyDowned += CaptureKeyBoardAndMouse_KeyDowned;
+		CaptureKeyBoardAndMouse.KeyUpped += CaptureKeyBoardAndMouse_KeyUpped;
+
+	}
+	private static void CaptureKeyBoardAndMouse_KeyDowned(object? sender, KeyEventArgs e)
+	{
+		KeyDowned(e.KeyCode);
+	}
+	private static void CaptureKeyBoardAndMouse_KeyUpped(object? sender, KeyEventArgs e)
+	{
+		KeyUpped(e.KeyCode);
+	}
+
+
+	private static void KeyDowned(Keys key)
+	{
+		try
+		{
+			if (key == Keys.LButton)
+				LeftMouseClicked?.Invoke();
+
+
+			if (_presetKeyCombination.Contains(key))
+			{
+				// Don't add it again if it's already there. This may happen when a user holds down a key.
+				if (!PressedKeys.Contains(key))
+					PressedKeys.Add(key);
+				Debug.WriteLine("Down:  " + key.ToString());
+
+				if (PressedKeys.Count == _presetKeyCombination.Count)
+					CheckHotKeyCombo();
+			}
+		}
+		catch (Exception ex)
+		{
+			Logs.Logger.Log(ex);
+		}
+	}
+
+	private static void KeyUpped(Keys key)
+	{
+		try
+		{
+			if (_presetKeyCombination.Contains(key))
+			{
+				PressedKeys.Remove(key);
+				Debug.WriteLine("Up: " + key.ToString());
+			}
+		}
+		catch (Exception ex)
+		{
+			Logs.Logger.Log(ex);
+		}
+	}
+
+
+	private static void CheckHotKeyCombo()
+	{
+		try
+		{
+
+			Debug.WriteLine("CHECKING");
+			bool allPressed = true;
+			foreach (Keys key in _presetKeyCombination)
+			{
+				if (!PressedKeys.Contains(key))
+					allPressed = false;
+			}
+			if (allPressed)
+			{
+				Debug.WriteLine("***** SHOWING THE WINDOW *****");
+				PressedKeys.Clear();
+				KeyCombinationHit?.Invoke();
+			}
+		}
+		catch (Exception ex)
+		{
+			Logs.Logger.Log(ex);
+		}
+	}
+
+	public delegate void KeyCombinationHitEventHandler();
+	public static event KeyCombinationHitEventHandler? KeyCombinationHit;
+
+	public delegate void LeftMouseClickEventHandler();
+	public static event LeftMouseClickEventHandler? LeftMouseClicked;
+}
---
+++ QuickPickNet6/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Ayazis.KeyHooks;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Application = System.Windows.Application;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace QuickPickNet6;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+
+		// Set Keyboard hooks
+		List<Keys> keyCombination = new List<Keys> { Keys.LMenu, Keys.RButton };
+
+
+		HotKeys.SubscribeToKeyEvents(keyCombination);
+
+		// Perform any initialization tasks here
+		// For example, you can create objects and set properties, or show a splash screen
+
+		// Create the main window and show it
+		var mainWindow = new MainWindow();
+		mainWindow.Show();
+
+
+		// Set Mainwindow Handle (needed for displaying thumbnails)
+
+	}
+}
