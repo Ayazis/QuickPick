096e1ec081f97c8dce094afcf2fd808e3ce9ef33
Ayazis <florisvdgeest@hotmail.com>
2023-04-29 12:57:15 +0200

    copying some filesover from older project

---
+++ QuickPickNet6/Models/QuickPickSettings.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickPick.Models
+{
+    public class QuickPickSettings
+    {
+        public QuickPickSettings(QuickPickModel qpm)
+        {
+            NrOfMainButtons = qpm.NrOfButtons;
+            ShortCutsFolder = qpm.ShortCutsFolder;
+            MainButtons = qpm.MainButtons;
+            InstantShortcuts = qpm.InstantShortCuts;
+        }
+
+        public QuickPickSettings()
+        {
+
+        }
+
+        public bool InstantShortcuts { get; set; }
+        public int NrOfMainButtons { get; set; }
+        public string ShortCutsFolder { get; set; }
+        public ObservableCollection<QpButton> MainButtons{ get; set; }
+
+    }
+}
