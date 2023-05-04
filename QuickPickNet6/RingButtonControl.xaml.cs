b4ebae4c3abc0f49323c5c9aef32157d73d2d9ed
Ayazis <florisvdgeest@hotmail.com>
2023-05-04 12:24:15 +0200

    Wip arced Button

--- QuickPickNet6/RingButtonControl.xaml.cs
+++ QuickPickNet6/RingButtonControl.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,55 @@ namespace QuickPick
         public RingButtonControl()
         {
             InitializeComponent();
+        }
+        private void BottomRight_MouseEnter(object sender, MouseEventArgs e)
+        {
+            QuadrantEnter(sender as Path);            
+        }
+
+      
+        private void BottomRight_MouseLeave(object sender, MouseEventArgs e)
+        {
+            QuadrantLeave(sender as Path);
+            // Code to execute when the mouse leaves the BottomRight Path element
+        }
+
+        private void BottomLeft_MouseEnter(object sender, MouseEventArgs e)
+        {
+            QuadrantEnter(sender as Path);
+        }
+
+        private void BottomLeft_MouseLeave(object sender, MouseEventArgs e)
+        {
+            QuadrantLeave(sender as Path);
+        }
+
+        private void TopLeft_MouseEnter(object sender, MouseEventArgs e)
+        {
+            QuadrantEnter(sender as Path);
+        }
+
+        private void TopLeft_MouseLeave(object sender, MouseEventArgs e)
+        {
+            QuadrantLeave(sender as Path);
+      }
+
+        private void TopRight_MouseEnter(object sender, MouseEventArgs e)
+        {
+            QuadrantEnter(sender as Path);
+        }
+
+        private void TopRight_MouseLeave(object sender, MouseEventArgs e)
+        {
+            QuadrantLeave(sender as Path);
+        }
+        private void QuadrantEnter(Path path)
+        {
+            path.Fill = Brushes.Black;
+        }
+        private void QuadrantLeave(Path path)
+        {
+            path.Fill = Brushes.Transparent;            
         }
     }
 }
