0fc0cf57b335dac178d492f345a681de171c3bea
Ayazis <florisvdgeest@hotmail.com>
2023-05-05 12:19:35 +0200

    Start JumbList thing

---
+++ Utilities/TaskBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Taskbar;
+
+public class PinnedAppManager
+{
+    private readonly string _categoryName;
+
+    public PinnedAppManager(string categoryName)
+    {
+        _categoryName = categoryName;
+    }
+
+    public JumpList GetJumbList()
+    {
+        // Get the JumpList for the application
+        JumpList jumpList = JumpList.CreateJumpList();        
+        return jumpList;
+    }
+}
