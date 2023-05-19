ad1514bc3f890f73572ae9564d48cd95c74ad676
Ayazis <florisvdgeest@hotmail.com>
2023-05-19 07:55:27 +0200

    fixed exception being thrown when getting currentDesktop while handling an event

--- Utilities/VirtualDesktopHelper.cs
+++ Utilities/VirtualDesktopHelper.cs
@@ -2,8 +2,20 @@
 
 public static class VirtualDesktopHelper
 {
-	public static bool IsWindowOnVirtualDesktop(IntPtr hwnd)
-	{
-		return WindowsDesktop.VirtualDesktop.IsCurrentVirtualDesktop(hwnd);
-	}
+    public static bool IsWindowOnVirtualDesktop(IntPtr hwnd)
+    {
+        bool isOnCurrentDesktop = false;
+        Thread thread = new Thread(() =>
+        {
+            isOnCurrentDesktop = WindowsDesktop.VirtualDesktop.IsCurrentVirtualDesktop(hwnd);
+            // Do something with the result
+        });
+
+        thread.SetApartmentState(ApartmentState.STA); // Set the thread's apartment state to STA
+        thread.Start(); // Start the thread
+
+        thread.Join(); // Wait for the thread to complete
+
+        return isOnCurrentDesktop;
+    }
 }
