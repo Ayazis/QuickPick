f68252786425ac7b1ae0c9aee0ed4474172e8706
Floris van de Geest <florisvdgeest@hotmail.com>
2023-05-20 14:47:29 +0200

    remove old code

--- QuickPickNet6/CircularPanel.cs
+++ QuickPickNet6/CircularPanel.cs
@@ -49,45 +49,4 @@ public class CircularPanel : Panel
 
 		return finalSize;
 	}
-
-
-	//protected override Size ArrangeOverride(Size finalSize)
-	//{
-	//	if (Children.Count == 0) return finalSize;
-
-	//	double angleStep = 7; // Set the desired angle step between buttons (in degrees)
-	//	double totalAngle = 360; // Set the total angle (in degrees) for the circle
-	//	double radius = Math.Min(finalSize.Width, finalSize.Height) / 2;
-
-	//	int numItems = Children.Count;
-	//	double maxAngleStep = totalAngle / numItems;
-	//	angleStep = Math.Min(maxAngleStep, angleStep);
-
-	//	// Calculate the minimum size required for each child
-	//	double minWidth = 0;
-	//	double minHeight = 0;
-
-	//	foreach (UIElement child in Children)
-	//	{
-	//		minWidth = Math.Max(minWidth, child.DesiredSize.Width);
-	//		minHeight = Math.Max(minHeight, child.DesiredSize.Height);
-	//	}
-
-	//	// Calculate the actual spacing based on the minimum size of the children
-	//	double actualSpacing = 2 * radius * Math.Sin((angleStep / 2) * (Math.PI / 180)) + Math.Max(minWidth, minHeight);	
-
-
-	//	double angle = -90;
-	//	foreach (UIElement child in Children)
-	//	{
-	//		double x = (finalSize.Width / 2) + (radius * Math.Cos(angle * Math.PI / 180)) - (child.DesiredSize.Width / 2);
-	//		double y = (finalSize.Height / 2) + (radius * Math.Sin(angle * Math.PI / 180)) - (child.DesiredSize.Height / 2);
-
-	//		child.Arrange(new Rect(new Point(x, y), child.DesiredSize));
-	//		angle += angleStep;
-	//		angle += (actualSpacing / radius) * (180 / Math.PI) -7; // Increase the angle by the desired spacing
-	//	}
-
-	//	return finalSize;
-	//}
 }
