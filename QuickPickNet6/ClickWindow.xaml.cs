703504082b0985da7c5d9c2d3538f4c572f29bf6
Ayazis <florisvdgeest@hotmail.com>
2023-06-01 09:08:47 +0200

    wip updating the layout before showing

--- QuickPickNet6/ClickWindow.xaml.cs
+++ QuickPickNet6/ClickWindow.xaml.cs
@@ -21,114 +21,122 @@ namespace QuickPick;
 /// </summary>
 public partial class ClickWindow : Window
 {
-	private static ClickWindow _instance;
-	private QuickPickMainWindowModel _qpm = new QuickPickMainWindowModel();
-	public Storyboard HideAnimation { get; private set; }
-	public Storyboard ShowAnimation { get; private set; }
-	public ClickWindow()
-	{
-		InitializeComponent();
-		DataContext = _qpm;
+    private static ClickWindow _instance;
+    private QuickPickMainWindowModel _qpm = new QuickPickMainWindowModel();
+    public Storyboard HideAnimation { get; private set; }
+    public Storyboard ShowAnimation { get; private set; }
+    public ClickWindow()
+    {
+        InitializeComponent();
+        DataContext = _qpm;
 
-		HideAnimation = TryFindResource("hideMe") as Storyboard;
-		ShowAnimation = TryFindResource("showMe") as Storyboard;
-
-
-
-		ShowWindowInvisible();
-		_instance = this;
-	}
-
-	private void HotKeys_LeftMouseClicked()
-	{
-		if (MouseIsOutsideWindow())
-			HideAnimation.Begin(this);
-	}
-	public bool MouseIsOutsideWindow()
-	{
-		var mouse = MousePosition.GetCursorPosition();
-
-		bool isOutside = (mouse.X < this.Left || mouse.X > this.Left + this.ActualWidth)
-						|| (mouse.Y < this.Top || mouse.Y > this.Top + this.ActualHeight);
-
-		return isOutside;
-	}
-	private void ShowWindowInvisible()
-	{
-		Opacity = 0;
-		Show();
-		Visibility = Visibility.Hidden;
-		Opacity = 1;
-	}
-
-	public void OnKeyCombinationHit()
-	{
-		var mousePosition = MousePosition.GetCursorPosition();
-
-		this.Left = mousePosition.X - (this.ActualWidth / 2);
-		this.Top = mousePosition.Y - (this.ActualHeight / 2);
-		ShowDialog();     // SHOW() throws engine exceptions!    
-						  //ShowWindow();
-						  //UpdateTaskbarShortCut();	
-	}
+        HideAnimation = TryFindResource("hideMe") as Storyboard;
+        ShowAnimation = TryFindResource("showMe") as Storyboard;
 
 
-	public void UpdateTaskbarShortCuts()
-	{
-		List<TaskbarShortCut> apps = TaskbarApps.GetPinnedAppsAndActiveWindows();
 
-		foreach (var app in apps)
-		{
-			var handle = ActiveWindows.GetActiveWindowOnCurentDesktop(app.TargetPath);
-			if (handle != default)
-				app.HasWindowActiveOnCurrentDesktop = true;
-		}
+        UpdateLayout();
+        _instance = this;
+    }
 
-		_qpm.PinnedApps = new ObservableCollection<TaskbarShortCut>(apps);
-		_qpm.NotifyPropertyChanged(nameof(_qpm.PinnedApps));
+    private void HotKeys_LeftMouseClicked()
+    {
+        if (MouseIsOutsideWindow())
+            HideAnimation.Begin(this);
+    }
+    public bool MouseIsOutsideWindow()
+    {
+        var mouse = MousePosition.GetCursorPosition();
 
-		this.Dispatcher.Invoke(() =>
-		{
-			this.UpdateLayout();
-			// make the UI refresh.... 
-		});
-	}
+        bool isOutside = (mouse.X < this.Left || mouse.X > this.Left + this.ActualWidth)
+                        || (mouse.Y < this.Top || mouse.Y > this.Top + this.ActualHeight);
 
-
-	public static void HideWindow()
-	{
-		try
-		{
-			_instance.Hide();
-			return;
-			_instance.HideAnimation.Begin(_instance);
-
-		}
-		catch (Exception ex)
-		{
-			Logs.Logger.Log(ex);
-		}
-	}
-
-	public void ShowWindow()
-	{
-		this.Show();
-		return;
+        return isOutside;
+    }
 
 
-		try
-		{
-			//SetActiveWindow();
-			//HideShortCuts();
-			var mousePosition = MousePosition.GetCursorPosition();
-			this.Left = mousePosition.X - (this.ActualWidth / 2);
-			this.Top = mousePosition.Y - (this.ActualHeight / 2);
-			ShowAnimation.Begin(this);
+    public void OnKeyCombinationHit()
+    {
+        ShowWindow();
+    }
 
-		}
-		catch (Exception ex)
-		{
-			Logs.Logger.Log(ex);
-		}
-	}
+
+    public void UpdateTaskbarShortCuts()
+    {
+        List<TaskbarShortCut> apps = TaskbarApps.GetPinnedAppsAndActiveWindows();
+
+        foreach (var app in apps)
+        {
+            var handle = ActiveWindows.GetActiveWindowOnCurentDesktop(app.TargetPath);
+            if (handle != default)
+                app.HasWindowActiveOnCurrentDesktop = true;
+        }
+
+        _qpm.PinnedApps = new ObservableCollection<TaskbarShortCut>(apps);
+        _qpm.NotifyPropertyChanged(nameof(_qpm.PinnedApps));
+
+
+        // Since we are not on the UI thread, use a dispatcher.
+        this.Dispatcher.Invoke(() =>
+        {
+            UpdateLayoutOfApplinks();
+        });
+    }
+
+    private void UpdateLayoutOfApplinks()
+    {
+        // Save original state
+        var originalState = WindowState;
+        var originalOpacity = Opacity;
+
+        Opacity = 0.01;
+
+        //  Visibility = Visibility.Hidden;        
+        //Visibility = Visibility.Visible;
+        //Opacity = 1;
+
+        Applinks.InvalidateMeasure();
+        Applinks.InvalidateVisual();
+        Applinks.UpdateLayout();
+        InvalidateMeasure();
+        InvalidateVisual();
+        UpdateLayout();
+        //Hide();        
+        //Show();
+
+        // Restore original state
+        Opacity = originalOpacity;    
+
+        //Hide();
+        //   Visibility = Visibility.Visible;
+    }
+    public static void HideWindow()
+    {
+        try
+        {
+            _instance.HideAnimation.Begin(_instance);
+
+        }
+        catch (Exception ex)
+        {
+            Logs.Logger.Log(ex);
+        }
+    }
+
+    public void ShowWindow()
+    {
+        try
+        {
+            Visibility = Visibility.Visible;
+            var mousePosition = MousePosition.GetCursorPosition();
+            this.Left = mousePosition.X - (this.ActualWidth / 2);
+            this.Top = mousePosition.Y - (this.ActualHeight / 2);
+            ShowAnimation.Begin(this);
+
+        }
+        catch (Exception ex)
+        {
+            Logs.Logger.Log(ex);
+        }
+    }
 }
