d362347bc66bb606b0266295b0a529ace5640717
Floris van de Geest <florisvdgeest@hotmail.com>
2023-08-22 09:29:03 +0200

    fixed showing loads of useless windows

--- Utilities/DesktopInterops/DesktopInterop.cs
+++ Utilities/DesktopInterops/DesktopInterop.cs
@@ -1,23 +1,47 @@
-﻿using System;
-using System.Linq;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace QuickPick.Utilities.DesktopInterops
 {
-    public class DesktopInterop
-    {
-        public static Guid GetCurrentDesktopGuid()
-        {
-            if (OsVersionChecker.IsWindows11Eligable)
-                return DesktopWin11.VirtualDesktopManagerInternal.GetCurrentDesktop(IntPtr.Zero).GetId();
-            else
-                return DesktopWin10.VirtualDesktopManagerInternal.GetCurrentDesktop().GetId();
-        }
-        public static bool IsWindowOnCurrentVirtualDesktop(IntPtr topLevelWindow)
-        {
-            if (OsVersionChecker.IsWindows11Eligable)
-                return DesktopWin11.VirtualDesktopManager.IsWindowOnCurrentVirtualDesktop(topLevelWindow);
-            else
-                return DesktopWin10.VirtualDesktopManager.IsWindowOnCurrentVirtualDesktop(topLevelWindow);
-        }
-    }
+	public class DesktopInterop
+	{
+		public static Guid GetCurrentDesktopGuid()
+		{
+			if (OsVersionChecker.IsWindows11Eligable)
+				return DesktopWin11.VirtualDesktopManagerInternal.GetCurrentDesktop(IntPtr.Zero).GetId();
+			else
+				return DesktopWin10.VirtualDesktopManagerInternal.GetCurrentDesktop().GetId();
+		}
+		public static bool IsWindowOnCurrentVirtualDesktop(IntPtr windowHandle)
+		{
+			string title = GetWindowTitle(windowHandle);
+			if (string.IsNullOrWhiteSpace(title))
+				return false;
+
+			if (OsVersionChecker.IsWindows11Eligable)
+				return DesktopWin11.VirtualDesktopManager.IsWindowOnCurrentVirtualDesktop(windowHandle);
+			else
+				return DesktopWin10.VirtualDesktopManager.IsWindowOnCurrentVirtualDesktop(windowHandle);
+		}
+		[DllImport("user32.dll", SetLastError = true)]
+		static extern int GetWindowTextLength(IntPtr hWnd);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+		public static string GetWindowTitle(IntPtr hWnd)
+		{
+			// Get the length of the window text
+			int length = GetWindowTextLength(hWnd);
+			if (length == 0) return null;
+
+			StringBuilder sb = new StringBuilder(length + 1);
+
+			// Get the window text
+			GetWindowText(hWnd, sb, sb.Capacity);
+
+			return sb.ToString();
+		}
+
+	}
 }
--- Utilities/VirtualDesktopHelper.cs
+++ Utilities/VirtualDesktopHelper.cs
@@ -1,87 +1,87 @@
-﻿//using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-//namespace Utilities.VirtualDesktop;
-
-
-
-//public interface IVirtualDesktopHelper
-//{
-//    Guid CurrentDesktopId { get; set; }    
-//    bool IsWindowOnVirtualDesktop(IntPtr hWnd, Guid currentVirtualDesktopId);
-//    void Dispose();
-//}
-
-//public class VirtualDesktopHelper : IVirtualDesktopHelper
-//{
-//    private IVirtualDesktopManager _virtualDesktopManager;
-//    bool _isDisposed;
-
-//    public VirtualDesktopHelper()
-//    {
-
-//        _virtualDesktopManager = (IVirtualDesktopManager)new VirtualDesktopManager();
-
-//    }
-
-//    public void Dispose()
-//    {
-
-//        if (_isDisposed)
-//            return;
-//        Marshal.ReleaseComObject(_virtualDesktopManager);
-//        _virtualDesktopManager = null;
-//        GC.SuppressFinalize(this);
-//        _isDisposed = true;
-
-//    }
-//    public Guid CurrentDesktopId { get; set; }
-//    public bool IsWindowOnVirtualDesktop(IntPtr hWnd, Guid currentVirtualDesktopId)
-//    {
-//        if (currentVirtualDesktopId == default)
-//            return false;
-
-//        Guid desktopIdForWindow = Guid.Empty;
-//        Thread newThread = new Thread(() =>
-//        {
-//            try
-//            {
-//                desktopIdForWindow = _virtualDesktopManager.GetWindowDesktopId(hWnd);
-//            }
-//            catch (COMException e)
-//            {
-//                // Random guid will return false.
-//                desktopIdForWindow = Guid.NewGuid();
-//            }
-//        });
-
-//        newThread.SetApartmentState(ApartmentState.STA); // Set the thread to STA, needed for some COM objects, and necessary in this case.
-//        newThread.Start();
-//        newThread.Join();
-//        return desktopIdForWindow == currentVirtualDesktopId;
-//    }
-
-//    #region ComImports
-//    /*
-//    * documentation: https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nn-shobjidl_core-ivirtualdesktopmanager
-//     */
-
-//    // Declare the interface for the virtual desktop manager
-//    [ComImport]
-//    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-//    [Guid("a5cd92ff-29be-454c-8d04-d82879fb3f1b")]
-//    private interface IVirtualDesktopManager
-//    {
-//        int IsWindowOnCurrentVirtualDesktop(IntPtr hWnd);
-//        Guid GetWindowDesktopId(IntPtr hWnd);
-//    }
-
-//    // Declare the virtual desktop manager
-//    [ComImport]
-//    [Guid("aa509086-5ca9-4c25-8f95-589d3c07b48a")]
-//    private class VirtualDesktopManager
-//    {
-//    }
+namespace Utilities.VirtualDesktop;
 
 
-//    #endregion
-//}
+
+public interface IVirtualDesktopHelper
+{
+    Guid CurrentDesktopId { get; set; }
+    bool IsWindowOnVirtualDesktop(IntPtr hWnd, Guid currentVirtualDesktopId);
+    void Dispose();
+}
+
+public class VirtualDesktopHelper : IVirtualDesktopHelper
+{
+    private IVirtualDesktopManager _virtualDesktopManager;
+    bool _isDisposed;
+
+    public VirtualDesktopHelper()
+    {
+
+        _virtualDesktopManager = (IVirtualDesktopManager)new VirtualDesktopManager();
+
+    }
+
+    public void Dispose()
+    {
+
+        if (_isDisposed)
+            return;
+        Marshal.ReleaseComObject(_virtualDesktopManager);
+        _virtualDesktopManager = null;
+        GC.SuppressFinalize(this);
+        _isDisposed = true;
+
+    }
+    public Guid CurrentDesktopId { get; set; }
+    public bool IsWindowOnVirtualDesktop(IntPtr hWnd, Guid currentVirtualDesktopId)
+    {
+        if (currentVirtualDesktopId == default)
+            return false;
+
+        Guid desktopIdForWindow = Guid.Empty;
+        Thread newThread = new Thread(() =>
+        {
+            try
+            {
+                desktopIdForWindow = _virtualDesktopManager.GetWindowDesktopId(hWnd);
+            }
+            catch (COMException e)
+            {
+                // Random guid will return false.
+                desktopIdForWindow = Guid.NewGuid();
+            }
+        });
+
+        newThread.SetApartmentState(ApartmentState.STA); // Set the thread to STA, needed for some COM objects, and necessary in this case.
+        newThread.Start();
+        newThread.Join();
+        return desktopIdForWindow == currentVirtualDesktopId;
+    }
+
+    #region ComImports
+    /*
+    * documentation: https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nn-shobjidl_core-ivirtualdesktopmanager
+     */
+
+    // Declare the interface for the virtual desktop manager
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("a5cd92ff-29be-454c-8d04-d82879fb3f1b")]
+    private interface IVirtualDesktopManager
+    {
+        int IsWindowOnCurrentVirtualDesktop(IntPtr hWnd);
+        Guid GetWindowDesktopId(IntPtr hWnd);
+    }
+
+    // Declare the virtual desktop manager
+    [ComImport]
+    [Guid("aa509086-5ca9-4c25-8f95-589d3c07b48a")]
+    private class VirtualDesktopManager
+    {
+    }
+
+
+    #endregion
+}
