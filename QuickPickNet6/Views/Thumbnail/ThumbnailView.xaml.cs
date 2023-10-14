909c6c04fe1332209d51fcd422afba31ee9f3841
Floris van de Geest <Developer_FG@outlook.com>
2023-10-14 12:54:15 +0200

    fix dpiscaling

--- QuickPickNet6/Views/Thumbnail/ThumbnailView.xaml.cs
+++ QuickPickNet6/Views/Thumbnail/ThumbnailView.xaml.cs
@@ -11,117 +11,117 @@ namespace QuickPick.UI.Views.Thumbnail;
 
 public partial class ThumbnailView : UserControl
 {
-	ThumbnailDataContext _context;
-	public ThumbnailView()
-	{
+    ThumbnailDataContext _context;
+    public ThumbnailView()
+    {
 
-	}
-	public ThumbnailView(ThumbnailDataContext context, double dpiScaling)
-	{
-		var size = CalculateRenderSize(context.Rect);
-		context.Width = size.Width * 1.25 * dpiScaling;
-		context.Height = size.Height * 1.25 * dpiScaling;
+    }
+    public ThumbnailView(ThumbnailDataContext context, double dpiScaling)
+    {
+        var size = CalculateRenderSize(context.Rect);
+        context.Width = size.Width * 1.25 / dpiScaling;
+        context.Height = size.Height * 1.25 / dpiScaling;
 
-		InitializeComponent();
-		this.DataContext = context;
-		_context = context;
-		this.Visibility = System.Windows.Visibility.Hidden;
-
-
-	}
-
-	public async Task FadeIn()
-	{
-		this.Dispatcher.Invoke(() =>
-		{
-			this.Opacity = 0;
-			this.Visibility = System.Windows.Visibility.Visible;
-
-		});
-
-		Task.Run(() => { ShowThumbnailViewAsync(); });
-		Task.Run(() => { ThumbnailCreator.CreateAndFadeInThumbnail(_context.ThumbnailRelation, _context.Rect); });
+        InitializeComponent();
+        this.DataContext = context;
+        _context = context;
+        this.Visibility = System.Windows.Visibility.Hidden;
 
 
-		return;
+    }
 
-		Task taskCreateAndFadeInThumbnail = new Task(() => { });
-		Task taskFadeInThisView = new Task(() => { });
-		taskFadeInThisView.Start();
-		taskCreateAndFadeInThumbnail.Start();
-		await Task.WhenAll(taskCreateAndFadeInThumbnail, taskFadeInThisView);
-	}
-	private async Task ShowThumbnailViewAsync(bool fadeIn = false)
-	{
-		if (fadeIn)
-		{
-			// use dispatchers as short as possible to prevent lag.
-			// Gradually increase the opacity over time to create a fade-in effect. // Same logic as in ThumbnailCreator
-			for (double i = 0; i <= 255; i += 25)
-			{
-				double newOpacityValue = i / 255;
-				double localOpacity = newOpacityValue;
-				this.Dispatcher.Invoke(() =>
-				{
-					this.Opacity = localOpacity;
-				});
-				// Sleep for a bit to control the speed of the fade-in. Adjust this value as needed.
-				Thread.Sleep(20);
-			}
-		}
-		this.Dispatcher.Invoke(() =>
-		{
-			// Make sure the opacity ends at 1 for full visibiblity.
-			this.Opacity = 1;
-		});
+    public async Task FadeIn()
+    {
+        this.Dispatcher.Invoke(() =>
+        {
+            this.Opacity = 0;
+            this.Visibility = System.Windows.Visibility.Visible;
+
+        });
+
+        Task.Run(() => { ShowThumbnailViewAsync(); });
+        Task.Run(() => { ThumbnailCreator.CreateAndFadeInThumbnail(_context.ThumbnailRelation, _context.Rect); });
 
 
-	}
+        return;
 
-	public void Hide()
-	{
-		this.Visibility = System.Windows.Visibility.Collapsed;
-		ThumbnailCreator.DwmUnregisterThumbnail(_context.ThumbnailRelation);
-	}
+        Task taskCreateAndFadeInThumbnail = new Task(() => { });
+        Task taskFadeInThisView = new Task(() => { });
+        taskFadeInThisView.Start();
+        taskCreateAndFadeInThumbnail.Start();
+        await Task.WhenAll(taskCreateAndFadeInThumbnail, taskFadeInThisView);
+    }
+    private async Task ShowThumbnailViewAsync(bool fadeIn = false)
+    {
+        if (fadeIn)
+        {
+            // use dispatchers as short as possible to prevent lag.
+            // Gradually increase the opacity over time to create a fade-in effect. // Same logic as in ThumbnailCreator
+            for (double i = 0; i <= 255; i += 25)
+            {
+                double newOpacityValue = i / 255;
+                double localOpacity = newOpacityValue;
+                this.Dispatcher.Invoke(() =>
+                {
+                    this.Opacity = localOpacity;
+                });
+                // Sleep for a bit to control the speed of the fade-in. Adjust this value as needed.
+                Thread.Sleep(20);
+            }
+        }
+        this.Dispatcher.Invoke(() =>
+        {
+            // Make sure the opacity ends at 1 for full visibiblity.
+            this.Opacity = 1;
+        });
 
-	private void UserControl_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-	{
-		var color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#101010");
 
-		// Set the fill color of the rectangle
-		SolidColorBrush fillBrush = new SolidColorBrush(color);
+    }
 
-		ThumbBackground.Background = fillBrush;
-		ClickWindow.ThumbnailTimer.StopTimer();
-	}
+    public void Hide()
+    {
+        this.Visibility = System.Windows.Visibility.Collapsed;
+        ThumbnailCreator.DwmUnregisterThumbnail(_context.ThumbnailRelation);
+    }
 
-	private void UserControl_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-	{
-		var color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#202020");
-		SolidColorBrush fillBrush = new SolidColorBrush(color);
+    private void UserControl_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#101010");
 
-		ThumbBackground.Background = fillBrush;
-		ClickWindow.ThumbnailTimer.StartTimer();
-		// starttimer
-	}
+        // Set the fill color of the rectangle
+        SolidColorBrush fillBrush = new SolidColorBrush(color);
 
-	private void UserControl_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-	{
-		ActiveWindows.ToggleWindow(_context.WindowHandle);
-		// Toggle.
-	}
-	public static System.Windows.Size CalculateRenderSize(RECT rect)
-	{
-		int width = rect.Right - rect.Left;
-		int height = rect.Bottom - rect.Top;
+        ThumbBackground.Background = fillBrush;
+        ClickWindow.ThumbnailTimer.StopTimer();
+    }
 
-		// Ensure width and height are positive
-		if (width < 0)
-			width = -width;
+    private void UserControl_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        var color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#202020");
+        SolidColorBrush fillBrush = new SolidColorBrush(color);
 
-		if (height < 0)
-			height = -height;
+        ThumbBackground.Background = fillBrush;
+        ClickWindow.ThumbnailTimer.StartTimer();
+        // starttimer
+    }
 
-		return new System.Windows.Size(width, height);
-	}
+    private void UserControl_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        ActiveWindows.ToggleWindow(_context.WindowHandle);
+        // Toggle.
+    }
+    public static System.Windows.Size CalculateRenderSize(RECT rect)
+    {
+        int width = rect.Right - rect.Left;
+        int height = rect.Bottom - rect.Top;
+
+        // Ensure width and height are positive
+        if (width < 0)
+            width = -width;
+
+        if (height < 0)
+            height = -height;
+
+        return new System.Windows.Size(width, height);
+    }
 }
