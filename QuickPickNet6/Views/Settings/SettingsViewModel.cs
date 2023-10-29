cc53f4e4cdca6397abd16bf770ea1295043eba2d
Floris van de Geest <F.van.de.Geest@peoplepower.nl>
2023-10-29 15:41:17 +0100

    Settings ViewModel as singleton

--- QuickPickNet6/Views/Settings/SettingsViewModel.cs
+++ QuickPickNet6/Views/Settings/SettingsViewModel.cs
@@ -13,11 +13,19 @@ public enum ActiveAppSetting
 
 public class SettingsViewModel : ObservableObject
 {
-    public SettingsViewModel()
-    {
-			
-    }
-    private AutoUpdateSetting _autoUpdateSetting = AutoUpdateSetting.Master;
+
+	private static SettingsViewModel _instance = new();
+
+	private SettingsViewModel()
+	{
+		// prevent construction outside this class.
+	}
+
+	public static SettingsViewModel Instance
+	{
+		get { return _instance; }  // needs explicit get accesor for xaml binding.
+	}
+	private AutoUpdateSetting _autoUpdateSetting = AutoUpdateSetting.Master;
 	private ActiveAppSetting _activeAppSetting = ActiveAppSetting.IncludePinnedTaskBarApps;
 
 	public AutoUpdateSetting AutoUpdateSetting
