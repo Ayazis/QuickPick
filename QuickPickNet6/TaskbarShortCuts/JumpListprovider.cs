afdc722497f1d67939af74bba1fc896d62b0bcd4
Floris van de Geest <Developer_FG@outlook.com>
2024-02-03 14:30:35 +0100

    wip getting files from jumplists

--- QuickPickNet6/TaskbarShortCuts/JumpListprovider.cs
+++ QuickPickNet6/TaskbarShortCuts/JumpListprovider.cs
@@ -3,16 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 namespace QuickPick
 {
     public class JumpListProvider
     {
-        string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        string recentFilesDir => Path.Combine(appData, @"Microsoft\Windows\Recent\AutomaticDestinations");
+        static JumpListProvider()
+        {
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            recentFilesDir = Path.Combine(appData, @"Microsoft\Windows\Recent\AutomaticDestinations");
+        }
+        static string recentFilesDir;
         public IEnumerable<AutomaticDestination> GetJumpList()
         {
-          var destinationFiles = new List<AutomaticDestination>();
+            var destinationFiles = new List<AutomaticDestination>();
 
             foreach (var filePath in Directory.GetFiles(recentFilesDir))
             {
@@ -20,7 +25,11 @@ namespace QuickPick
 
                 // todo: Find out which files are recently opened by which program.
                 destinationFiles.Add(jlist);
+                var filesOpened = jlist.DestListEntries.Select(s => s.Lnk);
             }
+
+
+
             return destinationFiles;
         }
     }
