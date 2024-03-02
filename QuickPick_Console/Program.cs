32bb7bede3ed72068664fbf905dcabd1010f4419
Floris van de Geest <Developer_FG@outlook.com>
2024-03-02 20:27:50 +0100

    removed unused namespaces

--- QuickPick_Console/Program.cs
+++ QuickPick_Console/Program.cs
@@ -1,15 +1,6 @@
-﻿using Ayazis.KeyHooks;
-using Ayazis.Utilities;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using QuickPick.UI.Views.Settings;
-using System.Windows;
-using System.Windows.Threading;
-using Utilities.Mouse_and_Keyboard;
-using Utilities.VirtualDesktop;
-
 namespace QuickPick;
-
 public class Program
 {
     [STAThread]
