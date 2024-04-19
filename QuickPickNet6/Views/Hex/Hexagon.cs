91ace84c2129948ed6aba8f528444304141080b8
Floris van de Geest <Developer_FG@outlook.com>
2024-04-19 14:26:10 +0200

    wip hexagonButton

--- QuickPickNet6/Views/Hex/Hexagon.cs
+++ QuickPickNet6/Views/Hex/Hexagon.cs
@@ -5,81 +5,103 @@ using System.Windows.Shapes;
 
 namespace QuickPick.UI.Views.Hex
 {
-	public class Hexagon : Shape
-	{
-		public enum Orientation
-		{
-			Horizontal,
-			Vertical
-		}
+    public class Hexagon : Shape
+    {
+        const string BorderBrush = "#6F6F6F";
+        Color BorderColor = (Color)ColorConverter.ConvertFromString(BorderBrush);
 
-		public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(
-			"Orientation", typeof(Orientation), typeof(Hexagon), new FrameworkPropertyMetadata(Orientation.Horizontal, FrameworkPropertyMetadataOptions.AffectsRender));
+        public Hexagon()
+        {
+            // Set the style directly in the constructor
+            Style = new Style(typeof(Hexagon))
+            {
+                Setters =
+            {
+                new Setter(Shape.FillProperty, new LinearGradientBrush
+                {
+                    StartPoint = new Point(0, 0),
+                    EndPoint = new Point(1, 1),
+                    GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop(Color.FromRgb(48, 48, 48), 1),
+                        new GradientStop(Color.FromRgb(67, 67, 67), 0.3)
+                    }
+                }),
+                new Setter(Shape.StrokeProperty, new SolidColorBrush(BorderColor)),
+                new Setter(Shape.StrokeThicknessProperty, 1.0)
+            }
+            };
 
-		public Orientation HexagonOrientation
-		{
-			get { return (Orientation)GetValue(OrientationProperty); }
-			set { SetValue(OrientationProperty, value); }
-		}
+        }
+        public enum Orientation
+        {
+            Horizontal,
+            Vertical
+        }
 
-		protected override Geometry DefiningGeometry
-		{
-			get
-			{
-				return new PathGeometry
-				{
-					Figures =
-					{
-						HexagonOrientation == Orientation.Horizontal ? CreateHorizontalHexagon() : CreateVerticalHexagon()
-					}
-				};
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(
+            "Orientation", typeof(Orientation), typeof(Hexagon), new FrameworkPropertyMetadata(Orientation.Horizontal, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public Orientation HexagonOrientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                return new PathGeometry
+                {
+                    Figures =
+                    {
+                        HexagonOrientation == Orientation.Horizontal ? CreateHorizontalHexagon() : CreateVerticalHexagon()
+                    }
+                };
+            }
+        }
+
+        private PathFigure CreateVerticalHexagon()
+        {
+            double radius = Width / 2;
+            double centerY = Height / 2;
+            var startPoint = new Point(radius * Math.Cos(Math.PI / 6) + radius, centerY + radius * Math.Sin(Math.PI / 6));
+            return new PathFigure
+            {
+                StartPoint = startPoint,
+                Segments =
+            {
+                new LineSegment {Point = new Point(radius * Math.Cos(Math.PI / 2) + radius, centerY + radius * Math.Sin(Math.PI / 2))},
+                new LineSegment {Point = new Point(radius * Math.Cos(5 * Math.PI / 6) + radius, centerY + radius * Math.Sin(5 * Math.PI / 6))},
+                new LineSegment {Point = new Point(radius * Math.Cos(7 * Math.PI / 6) + radius, centerY + radius * Math.Sin(7 * Math.PI / 6))},
+                new LineSegment {Point = new Point(radius * Math.Cos(3 * Math.PI / 2) + radius, centerY + radius * Math.Sin(3 * Math.PI / 2))},
+                new LineSegment {Point = new Point(radius * Math.Cos(11 * Math.PI / 6) + radius, centerY + radius * Math.Sin(11 * Math.PI / 6))},
+                new LineSegment {Point = startPoint} // Add this line to close the hexagon.
 			}
-		}
+            };
+        }
+        private PathFigure CreateHorizontalHexagon()
+        {
+            double radius = Width / 2;
+            double centerX = Width / 2;
+            double centerY = Height / 2;
 
-		private PathFigure CreateVerticalHexagon()
-		{
-			double radius = Width / 2;
-			double centerY = Height / 2;
-			var startPoint = new Point(radius * Math.Cos(Math.PI / 6) + radius, centerY + radius * Math.Sin(Math.PI / 6));
-			return new PathFigure
-			{
-				StartPoint = startPoint,
-				Segments =
-			{
-				new LineSegment {Point = new Point(radius * Math.Cos(Math.PI / 2) + radius, centerY + radius * Math.Sin(Math.PI / 2))},
-				new LineSegment {Point = new Point(radius * Math.Cos(5 * Math.PI / 6) + radius, centerY + radius * Math.Sin(5 * Math.PI / 6))},
-				new LineSegment {Point = new Point(radius * Math.Cos(7 * Math.PI / 6) + radius, centerY + radius * Math.Sin(7 * Math.PI / 6))},
-				new LineSegment {Point = new Point(radius * Math.Cos(3 * Math.PI / 2) + radius, centerY + radius * Math.Sin(3 * Math.PI / 2))},
-				new LineSegment {Point = new Point(radius * Math.Cos(11 * Math.PI / 6) + radius, centerY + radius * Math.Sin(11 * Math.PI / 6))},
-				new LineSegment {Point = startPoint} // Add this line to close the hexagon.
-			}
-			};
-		}
-		private PathFigure CreateHorizontalHexagon()
-		{
-			double radius = Width / 2;
-			double centerX = Width / 2;
-			double centerY = Height / 2;
-
-			var startPoint = new Point(centerX + radius * Math.Cos(0), centerY + radius * Math.Sin(0));
-			return new PathFigure
-			{
-				StartPoint = startPoint,
-				Segments =
-				{
-					new LineSegment {Point = new Point(centerX + radius * Math.Cos(Math.PI / 3), centerY + radius * Math.Sin(Math.PI / 3))},
-					new LineSegment {Point = new Point(centerX + radius * Math.Cos(2 * Math.PI / 3), centerY + radius * Math.Sin(2 * Math.PI / 3))},
-					new LineSegment {Point = new Point(centerX + radius * Math.Cos(Math.PI), centerY + radius * Math.Sin(Math.PI))},
-					new LineSegment {Point = new Point(centerX + radius * Math.Cos(4 * Math.PI / 3), centerY + radius * Math.Sin(4 * Math.PI / 3))},
-					new LineSegment {Point = new Point(centerX + radius * Math.Cos(5 * Math.PI / 3), centerY + radius * Math.Sin(5 * Math.PI / 3))},
-					new LineSegment {Point = startPoint} // Add this line to close the hexagon.
+            var startPoint = new Point(centerX + radius * Math.Cos(0), centerY + radius * Math.Sin(0));
+            return new PathFigure
+            {
+                StartPoint = startPoint,
+                Segments =
+                {
+                    new LineSegment {Point = new Point(centerX + radius * Math.Cos(Math.PI / 3), centerY + radius * Math.Sin(Math.PI / 3))},
+                    new LineSegment {Point = new Point(centerX + radius * Math.Cos(2 * Math.PI / 3), centerY + radius * Math.Sin(2 * Math.PI / 3))},
+                    new LineSegment {Point = new Point(centerX + radius * Math.Cos(Math.PI), centerY + radius * Math.Sin(Math.PI))},
+                    new LineSegment {Point = new Point(centerX + radius * Math.Cos(4 * Math.PI / 3), centerY + radius * Math.Sin(4 * Math.PI / 3))},
+                    new LineSegment {Point = new Point(centerX + radius * Math.Cos(5 * Math.PI / 3), centerY + radius * Math.Sin(5 * Math.PI / 3))},
+                    new LineSegment {Point = startPoint} // Add this line to close the hexagon.
 				}
-			};
-		}
+            };
+        }
 
-	}
-
-
+    }
 }
-
-
---
+++ QuickPickNet6/Views/Hex/HexagonButton.cs
@@ -0,0 +1,60 @@
+﻿using FontAwesome5;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace QuickPick.UI.Views.Hex;
+
+public class HexagonButton : Button
+{
+    Hexagon _hexagon;
+    ImageAwesome _icon;
+
+    double hexScale = .9;
+    double iconScale = .3;
+    public HexagonButton()
+    {
+        this.BorderThickness = new System.Windows.Thickness(0);
+        Padding = new System.Windows.Thickness(0);
+        Background = Brushes.Transparent;
+
+        // Create a Grid to hold the Hexagon and the icon
+        Grid grid = new Grid();
+
+        // Create the Hexagon shape
+        _hexagon = new Hexagon()
+        {
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+            VerticalAlignment = System.Windows.VerticalAlignment.Center,
+        };
+
+        // Add the Hexagon to the Grid
+        grid.Children.Add(_hexagon);
+
+        // Create the FontAwesome icon
+        _icon = new ImageAwesome
+        {
+            Icon = EFontAwesomeIcon.Solid_InfoCircle,
+            Foreground = Brushes.White,
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+            VerticalAlignment = System.Windows.VerticalAlignment.Center,
+            Width = this.Width * iconScale,
+        };
+
+        // Add the icon to the Grid
+        grid.Children.Add(_icon);
+
+        // Set the Content of the Button to the Grid
+        this.Content = grid;
+
+        SizeChanged += HexagonButton_SizeChanged; ;
+    }
+
+    private void HexagonButton_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+    {
+        // Set the Width and Height of the Hexagon to be the same as the ActualWidth and ActualHeight of the HexagonButton
+        _hexagon.Width = this.ActualWidth * hexScale;
+        _hexagon.Height = this.ActualHeight * hexScale;
+
+        _icon.Width = this.ActualWidth * iconScale;
+    }
+}
