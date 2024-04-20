7f7b01ad919afafa4de470b715efafb9185cc9d1
Floris van de Geest <Developer_FG@outlook.com>
2024-04-20 13:20:12 +0200

    wip hexbuttons

--- QuickPickNet6/Views/Hex/HexCenter.xaml.cs
+++ QuickPickNet6/Views/Hex/HexCenter.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickPick.UI.Views.Settings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,4 +24,27 @@ public partial class HexCenter : UserControl
 	{
 		InitializeComponent();
 	}
+
+    private void HexCenter_Click(object sender, RoutedEventArgs e)
+    {
+        SettingsWindow.Instance.ShowWindow();
+        SettingsWindow.Instance.Activate();
+        SettingsWindow.Instance.Focus();
+        ClickWindow.Instance.HideUI();
+    }
+
+    private void Hex1_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Hex2_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void Hex3_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
 }
