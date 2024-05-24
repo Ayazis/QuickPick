fa4562df8d71fd3abcfc938d2d3f41a4671df3ad
Floris van de Geest <Developer_FG@outlook.com>
2024-05-24 21:17:40 +0200

    added next and previous song buttons

--- QuickPickNet6/Views/Hex/HexCenter.xaml.cs
+++ QuickPickNet6/Views/Hex/HexCenter.xaml.cs
@@ -46,9 +46,11 @@ public partial class HexCenter : UserControl
     {
         _hexes[0].HexButton.AsSettingsButton();
         _hexes[1].HexButton.AsShowDesktopButton();
-        _hexes[2].HexButton.AsBrightnessControl();
-        _hexes[3].HexButton.AsVolumeControl();
-        _hexes[4].HexButton.AsPlayPauseToggle();
+        _hexes[2].HexButton.AsNextSong();
+        _hexes[3].HexButton.AsPlayPauseToggle();
+        _hexes[4].HexButton.AsPreviousSong();
+        _hexes[5].HexButton.AsVolumeControl();        
+        _hexes[6].HexButton.AsBrightnessControl();
     }
 
 
--- Utilities/Mouse and Keyboard/InputSim.cs
+++ Utilities/Mouse and Keyboard/InputSim.cs
@@ -3,59 +3,67 @@ using WindowsInput.Native;
 
 namespace QuickPick.Logic
 {
-	public static class InputSim
-	{
-		public static InputSimulator Simulator = new InputSimulator();
+    public static class InputSim
+    {
+        public static InputSimulator Simulator = new InputSimulator();
 
-		public static void WinD()
-		{
-			   Simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_D);
-		}
+        public static void WinD()
+        {
+            Simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_D);
+        }
 
 
         public static void CtrlAltBreak()
-		{
-			Simulator.Keyboard.ModifiedKeyStroke(new VirtualKeyCode[]
-			{
-				VirtualKeyCode.LCONTROL,
-				VirtualKeyCode.MENU,
-				VirtualKeyCode.PAUSE
-			}, VirtualKeyCode.NONAME);
-		}
+        {
+            Simulator.Keyboard.ModifiedKeyStroke(new VirtualKeyCode[]
+            {
+                VirtualKeyCode.LCONTROL,
+                VirtualKeyCode.MENU,
+                VirtualKeyCode.PAUSE
+            }, VirtualKeyCode.NONAME);
+        }
 
 
-		public static void VolummeUp()
-		{
-			Simulator.Keyboard.KeyPress(VirtualKeyCode.VOLUME_UP);
-		}
+        public static void VolummeUp()
+        {
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.VOLUME_UP);
+        }
 
-		public static void VolummeDown()
-		{
-			Simulator.Keyboard.KeyPress(VirtualKeyCode.VOLUME_DOWN);
-		}
-		public static void CtrlN()
-		{
-			Simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LCONTROL, VirtualKeyCode.VK_N);
-		}
+        public static void VolummeDown()
+        {
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.VOLUME_DOWN);
+        }
+        public static void CtrlN()
+        {
+            Simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LCONTROL, VirtualKeyCode.VK_N);
+        }
 
-		public static void Paste()
-		{
-			Simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LCONTROL, VirtualKeyCode.VK_V);
-		}
+        public static void Paste()
+        {
+            Simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LCONTROL, VirtualKeyCode.VK_V);
+        }
 
-		public static void F5()
-		{
-			Simulator.Keyboard.KeyDown(VirtualKeyCode.F5);
-		}
+        public static void F5()
+        {
+            Simulator.Keyboard.KeyDown(VirtualKeyCode.F5);
+        }
 
-		public static void PlayPause()
-		{
-			Simulator.Keyboard.KeyPress(VirtualKeyCode.MEDIA_PLAY_PAUSE);
-		}
+        public static void PlayPause()
+        {
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.MEDIA_PLAY_PAUSE);
+        }
 
-		public static void ToggleMute()
-		{
-			Simulator.Keyboard.KeyPress(VirtualKeyCode.VOLUME_MUTE);
-		}		
-	}
+        public static void ToggleMute()
+        {
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.VOLUME_MUTE);
+        }
+        public static void PreviousSong()
+        {
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.MEDIA_PREV_TRACK);
+        }
+        public static void NextSong()
+        {
+            Simulator.Keyboard.KeyPress(VirtualKeyCode.MEDIA_NEXT_TRACK);
+        }
+    }
 }
