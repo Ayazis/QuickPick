384ad5fe73573bd61fca4c1091fc9618a95ca0a8
Floris van de Geest <Developer_FG@outlook.com>
2024-06-01 15:09:33 +0200

    bt client

---
+++ Utilities/Bluetooth/BluetoothDeviceManager.cs
@@ -0,0 +1,30 @@
+﻿using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.Collections.Generic;
+
+public class BluetoothManager
+{
+    private BluetoothClient _bluetoothClient = new BluetoothClient();
+
+    public List<string> GetPairedDevices()
+    {
+        return _bluetoothClient.PairedDevices.Select(s => s.DeviceName).ToList();
+    }
+
+    public bool ConnectToDevice(string deviceName)
+    {
+        try
+        {
+            var device = _bluetoothClient.PairedDevices.FirstOrDefault(s => s.DeviceName == deviceName);
+            if (device == null)
+                return false;
+
+            _bluetoothClient.Connect(device.DeviceAddress, BluetoothService.SerialPort);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
